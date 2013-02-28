1a5a4c62983ba0342527a0359e8cb15c6b47fe78
Daniel Wertheim <daniel@wertheim.se>
2013-02-28 20:54:51 +0100

    Go with fake as a default.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var config = require('./lib/config'),
-	importer = require('./lib/jsonimporter'),
+	importer = require('./lib/jsonimporter.fake'),
 	reporter = require('./lib/reporter');
 	
 importer.import(function (json) {
